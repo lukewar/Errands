f219a6a8511aecabc310a2a0b814beae5d39abba
Lukasz Warchol <lwarchol87@gmail.com>
2018-06-11 17:50:59 +0200

    Swiftformat updates.

--- Errands/Errands.swift
+++ Errands/Errands.swift
@@ -9,57 +9,57 @@ typealias OperationClosure<T, U> = (T, @escaping DoneClosure<U>) -> Void
 
 class Errands {
     fileprivate var anchorTask: Task<Void, Void>?
-    
+
     func first<O>(_ operation: @escaping (@escaping DoneClosure<O>) -> Void) -> Task<Void, O> {
-        guard self.anchorTask == nil else {
+        guard anchorTask == nil else {
             preconditionFailure("Errands setup more than once.")
         }
-        
-        anchorTask = Task<Void, Void>(errands: self){ _, done in done(()) }
-        
+
+        anchorTask = Task<Void, Void>(errands: self) { _, done in done(()) }
+
         let nextTask = Task<Void, O>(errands: self) { _, done in
             operation(done)
         }
         anchorTask!.bind(nextTask: nextTask)
-        
+
         return nextTask
     }
 }
 
 class Task<I, O> {
     let operation: OperationClosure<I, O>
-    
+
     private(set) var errands: Errands?
     private var completion: (DoneClosure<O>)?
-    
+
     init(errands: Errands, operation: @escaping OperationClosure<I, O>) {
         self.errands = errands
         self.operation = operation
-        self.completion = nil
+        completion = nil
     }
-    
+
     func then<T>(_ operation: @escaping OperationClosure<O, T>) -> Task<O, T> {
         guard let errands = self.errands else {
             preconditionFailure("Errands already run.")
         }
-        guard self.completion == nil else {
+        guard completion == nil else {
             preconditionFailure("Task already binded.")
         }
-        
+
         let nextTask = Task<O, T>(errands: errands, operation: operation)
         bind(nextTask: nextTask)
         return nextTask
     }
-    
+
     fileprivate func bind<T>(nextTask task: Task<O, T>) {
         completion = { [weak self] output in
             self?.errands = nil
             task.execute(input: output)
         }
     }
-    
+
     private func execute(input: I) {
-        guard self.errands != nil else {
+        guard errands != nil else {
             preconditionFailure("Errands already run.")
         }
         guard let completion = completion else {
@@ -67,7 +67,7 @@ class Task<I, O> {
         }
         operation(input, completion)
     }
-    
+
     func finally(_ completion: @escaping DoneClosure<Void>) {
         guard let errands = self.errands else {
             preconditionFailure("Errands already completed.")
@@ -75,14 +75,14 @@ class Task<I, O> {
         guard let anchorTask = errands.anchorTask else {
             fatalError()
         }
-        
+
         let lastTask = Task<O, Void>(errands: errands)
         lastTask.completion = { arg in
             lastTask.errands = nil
             completion(arg)
         }
-        self.bind(nextTask: lastTask)
-        
+        bind(nextTask: lastTask)
+
         anchorTask.execute(input: ())
     }
 }
--- ErrandsTests/ErrandsTests.swift
+++ ErrandsTests/ErrandsTests.swift
@@ -2,30 +2,30 @@
 //  Copyright © 2018 Lukasz Warchol. All rights reserved.
 //
 
-import XCTest
 @testable import Errands
+import XCTest
 
 class ErrandsTests: XCTestCase {
     var sut: Errands!
-    
+
     override func tearDown() {
         sut = nil
         super.tearDown()
     }
-    
+
     func testSyncronousExecution() {
         sut = Errands()
         sut.first { (done: @escaping DoneClosure<Void>) in
             print("begin")
             done(())
-        }.then() { (_, done: @escaping DoneClosure<String>) in
+        }.then { (_, done: @escaping DoneClosure<String>) in
             print("step 2")
             done("completed")
         }.finally {
             print("done")
         }
     }
-    
+
     func testAsyncronousExecution() {
         let expectation = XCTestExpectation(description: "Asynchronous expectation")
 
